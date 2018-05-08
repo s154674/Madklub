589de420360b504409a559e8f4b9be04373bfe71
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2018-05-08 12:50:50 +0200

    no message

--- html/js/maddage.js
+++ html/js/maddage.js
@@ -5,10 +5,15 @@ function initMaddage(){
 
 $("#content-maddage table tbody button.attend").click(function(){
     $(this).hide();
+    $(this).parent("td").parent("tr").find("a.plus").show();
+    $(this).parent("td").parent("tr").find("a.late").show();
     $(this).parent("td").find("button.disattend").show();
+
 });
 
 $("#content-maddage table tbody button.disattend").click(function(){
     $(this).hide();
+    $(this).parent("td").parent("tr").find("a.plus").hide();
+    $(this).parent("td").parent("tr").find("a.late").hide();
     $(this).parent("td").find("button.attend").show();
 });
